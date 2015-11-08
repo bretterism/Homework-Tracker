ee66a15dbdcc94fb2cebc0242ef3b8dff394eaf0
bretterism <bretterism@gmail.com>
2015-11-08 15:59:04 -0800

    Forgot to add the new files from the last commit.

---
+++ app/database/conn.js
@@ -0,0 +1,12 @@
+/*
+ * conn.js
+ * Database connection credentials.
+ * Don't share with the outside world!
+ */
+
+module.exports = {
+	host : 'localhost',
+	user : 'hw_user',
+	password : 'pass',
+	database : 'homework_tracker_db'
+};
