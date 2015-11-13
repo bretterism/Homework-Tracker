d5af0f101982e6e4d0af3c7d1981b3d086eed104
bretterism <bretterism@gmail.com>
2015-11-13 10:08:47 -0800

    added conn file again.

--- app/database/conn.js
+++ app/database/conn.js
@@ -5,13 +5,8 @@
  */
 
 module.exports = {
-	// host : 'localhost',
-	// user : 'hw_user',
-	// password : 'pass',
-	// database : 'homework_tracker_db'
-	host     : 'aatzr66md35cr6.c8t6xhid1m4s.us-west-2.rds.amazonaws.com',
-	user     : 'rds_user',
-	password : 'rds_pass123',
-	database : 'homework_tracker_db',
-	port     : '3306'
+	host : 'localhost',
+	user : 'hw_user',
+	password : 'pass',
+	database : 'homework_tracker_db'
 };
